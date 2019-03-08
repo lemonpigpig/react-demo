101199d760255a941f98f0d3beeffab5403cb968
lh03 <hexianjuan@gznb.com>
2019-03-08 19:35:39 +0800

    feat:add mobs

--- src/mobx/index.js
+++ src/mobx/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 @decorator
 class a {}
 function decorator(target) {
@@ -6,13 +8,25 @@ function decorator(target) {
 }
 const aa = new a()
 console.log('---aa----:', aa)
+const appState = observable({
+  counter: 0
+})
+console.log('---appState----:', appState)
+appState.increment = function() {
+  this.counter++
+}
+@observer
 class Mobx extends Component {
   render() {
     return (
       <div className="Mobx">
-        <div>Mobx</div>
+        <div>Mobx:{appState.counter}</div>
+        <button onClick={this.increment}>click increment</button>
       </div>
     )
+  }
+  increment = () => {
+    appState.increment()
   }
 }
 
